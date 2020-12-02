752d864c87c224556ae9d64858c0e250968f7480
ELLICOTTCITY <67174778+ELLICOTTCITY@users.noreply.github.com>
2020-12-01 19:09:58 -0500

    Create main.tf

---
+++ ec2/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "example-key"
+  public_key = file("ec2.pub")
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+resource "aws_instance" "ec2_instance" {
+  ami                    = "ami-04d29b6f966df1537"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name               = aws_key_pair.example.id
+  tags = {
+    Name = "my-first-ec2-instance"
+  }
+}
